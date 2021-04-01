ae1439f90b177444f2dee14584792958f7f3f53b
Charlyln <charleshenry.lenue>
2021-04-01 15:33:42 +0200

    add the 3 other routes to router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import NavBar from './components/NavBar'
-import Home from './Home'
+import NavBar from './components/navbar/NavBar'
+import Home from './components/home/Home'
+import Live from './components/live/Live'
+import Funzone from './components/funzone/Funzone'
+import Testimonials from './components/testimonials/Testimonials'
 
 function Router() {
   return (
@@ -9,6 +12,9 @@ function Router() {
       <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/live" component={Live} />
+        <Route path="/funzone" component={Funzone} />
+        <Route path="/testimonials" component={Testimonials} />
       </Switch>
     </BrowserRouter>
   )
