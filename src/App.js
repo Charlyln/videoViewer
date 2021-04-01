c3ae7f2bc50fc96566a2a136bc31445f59383bb0
Charlyln <charleshenry.lenue>
2021-04-01 11:15:30 +0200

    create first query with useQuery

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { gql, useQuery } from '@apollo/client'
 import React from 'react'
 import './App.css'
 
 function App() {
+  const { loading, error, data } = useQuery(gql`
+    {
+      allVideos(limit: 5) {
+        items {
+          name
+        }
+      }
+    }
+  `)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error</p>
+
+  console.log(data)
+
   return (
     <div className="App">
-      <header className="App-header">
-       {`Hello world ! `}
-      </header>
+      <header className="App-header">{`Hello world ! `}</header>
     </div>
   )
 }
