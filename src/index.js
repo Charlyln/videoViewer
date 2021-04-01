4364bf21b3a5f0f50c951fae57741032be0c5481
Charlyln <charleshenry.lenue>
2021-04-01 11:20:22 +0200

    add account key in header

--- src/index.js
+++ src/index.js
@@ -8,9 +8,15 @@ import {
 import './index.css'
 import App from './App'
 
+const key = 'x-account-key'
+const keyValue = 'SyT0uHf3I'
+
 const client = new ApolloClient({
   uri: 'https://staging-graphql-service.onrewind.tv/graphql',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  headers: {
+    [key]: keyValue
+  }
 })
 
 ReactDOM.render(
