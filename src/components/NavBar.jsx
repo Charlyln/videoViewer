d878179a1711bd80549040fa501b23054acdce71
Charlyln <charleshenry.lenue>
2021-04-01 14:44:24 +0200

    use a map function to display links in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from './NavBar.module.css'
 
 function NavBar() {
   return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/">Live</Link>
-      </li>
-      <li>
-        <Link to="/">Funzone</Link>
-      </li>
-      <li>
-        <Link to="/">Testimonials</Link>
-      </li>
+    <ul className={styles.ul}>
+      {links.map((link) => (
+        <li key={link.name}>
+          <Link to={link.path}>{link.name}</Link>
+        </li>
+      ))}
     </ul>
   )
 }
+
+const links = [
+  {
+    path: '/',
+    name: 'Home'
+  },
+  {
+    path: '/live',
+    name: 'Live'
+  },
+  {
+    path: '/funzone',
+    name: 'Funzone'
+  },
+  {
+    path: '/testimonials',
+    name: 'Testimonials'
+  }
+]
 
 export default NavBar
