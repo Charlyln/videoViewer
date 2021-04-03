62cec7c7a565af69ab8128aeb0fae18d201aaee8
Charlyln <charleshenry.lenue>
2021-04-03 22:48:30 +0200

    add link to video  details in videoCard

--- src/components/utils/VideoCard.jsx
+++ src/components/utils/VideoCard.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import style from './VideoCard.module.css'
 
 function VideoCard({ video }) {
-  const { name, poster, Tags } = video
+  const { name, poster, Tags, id } = video
 
   return (
     <div className={style.card}>
-      <img className={style.img} src={poster} alt={name} />
-      <div>
-        <h4>
-          <b>{name}</b>
-        </h4>
-        {Tags.map((tag) => (
-          <p key={tag.id}>{tag.name}</p>
-        ))}
-      </div>
+      <Link to={`/video/${id}`}>
+        <img
+          className={style.img}
+          src={poster || 'https://via.placeholder.com/300x168'}
+          alt={name}
+        />
+        <div>
+          <h4>
+            <b>{name}</b>
+          </h4>
+          {Tags.map((tag) => (
+            <p key={tag.id}>{tag.name}</p>
+          ))}
+        </div>
+      </Link>
     </div>
   )
 }
