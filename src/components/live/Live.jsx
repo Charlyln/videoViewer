1911cc17456d730443211a6cbaa9d3202c786876
Charlyln <charleshenry.lenue>
2021-04-03 22:46:39 +0200

    add video player in Live with video.js

--- src/components/live/Live.jsx
+++ src/components/live/Live.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import videojs from 'video.js'
 
 function Live() {
-  return ''
+  const videoSrc =
+    'https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8'
+  const playerRef = useRef()
+  const videoJSOptions = {
+    autoplay: true,
+    controls: false
+  }
+
+  useEffect(() => {
+    const player = videojs(playerRef.current, videoJSOptions, () => {
+      player.src(videoSrc)
+    })
+  })
+
+  return (
+    <div>
+      <video ref={playerRef} />
+    </div>
+  )
 }
+
 export default Live
