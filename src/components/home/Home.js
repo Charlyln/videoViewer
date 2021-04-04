dcbeccd42b6210470f084889bb4e44be6b198c52
Charlyln <charleshenry.lenue>
2021-04-04 16:46:57 +0200

    add fetchMore function to fetch new videos in home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,12 @@
-import { gql, useQuery } from '@apollo/client'
+import { gql } from 'apollo-boost'
 import React from 'react'
 import VideoCard from '../utils/VideoCard'
+import { useQuery } from '@apollo/react-hooks'
 
 function Home() {
-  const { loading, error, data } = useQuery(gql`
-    {
-      allVideos(limit: 5) {
+  const ALLVIDEOS_LIMIT_5 = gql`
+    query AllLimit5($after: String) {
+      allVideos(limit: 5, after: $after) {
         items {
           id
           name
@@ -15,21 +16,45 @@ function Home() {
             name
           }
         }
+        cursor {
+          before
+          after
+        }
       }
     }
-  `)
+  `
+
+  const { loading, error, data, fetchMore } = useQuery(ALLVIDEOS_LIMIT_5)
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error</p>
 
-  console.log(data)
+  const fetchMoreVideos = () => {
+    fetchMore({
+      variables: {
+        after: data.allVideos.cursor.after
+      },
+      updateQuery: (prevResult, { fetchMoreResult }) => {
+        fetchMoreResult.allVideos.items = [...fetchMoreResult.allVideos.items]
+        return fetchMoreResult
+      }
+    })
+  }
 
   return (
-    <div className="cardsContainer">
-      {data.allVideos.items.map((video) => (
-        <VideoCard video={video} key={video.id} />
-      ))}
-    </div>
+    <>
+      <div className="cardsContainer">
+        {data.allVideos.items.map((video) => (
+          <VideoCard video={video} key={video.id} />
+        ))}
+      </div>
+      <button
+        className=" hover:text-gray-300 hover:bg-gray-500 bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium "
+        onClick={fetchMoreVideos}
+      >
+        More
+      </button>
+    </>
   )
 }
 
