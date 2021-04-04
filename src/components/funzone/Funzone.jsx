af29f6ba4ca5e8eaedfed2cbb65008c4ee698857
Charlyln <charleshenry.lenue>
2021-04-04 12:21:04 +0200

    add gql request in funzone and display video cards

--- src/components/funzone/Funzone.jsx
+++ src/components/funzone/Funzone.jsx
@@ -1,6 +1,33 @@
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+import VideoCard from '../utils/VideoCard'
 
 function Funzone() {
-  return <p>Funzone</p>
+  const { loading, error, data } = useQuery(gql`
+    {
+      allVideos(tags: "Funzone", limit: 5) {
+        items {
+          id
+          name
+          poster
+          Tags {
+            id
+            name
+          }
+        }
+      }
+    }
+  `)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error</p>
+
+  return (
+    <div className="cardsContainer">
+      {data.allVideos.items.map((video) => (
+        <VideoCard video={video} key={video.id} />
+      ))}
+    </div>
+  )
 }
 export default Funzone
